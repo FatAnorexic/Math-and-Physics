f562664bb020cb0f5d1c0adff129853c0420e6d8
FatAnorexic <alexbernhardt23@gmail.com>
2023-07-09 23:22:51 -0400

    Added doctest mod, and better explanation

--- Misc/fibonacci_series.py
+++ Misc/fibonacci_series.py
@@ -5,10 +5,13 @@ def main():
     for i in range(1,400):
         print(fib(i))
 
-#This is one of the cache decorators->we're using the last 5 values in memory
+#This is one of the cache decorators->we're using the last 3 values in memory
 #By doing so, we greatly improve the speed at which our code executes.
+#There is a limit to how big n can be. The recursion depth limit is default
+#at 1000. This can be increased by sys.setrecursionlimit(n), but is highly not
+#reccomended.
 
-@lru_cache(maxsize=5)
+@lru_cache(maxsize=3)
 def fib(n):
     if n==0:
         return 0
@@ -16,4 +19,9 @@ def fib(n):
         return 1
     else:
         return fib(n-1)+fib(n-2)
-main()
+
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
+    main()
