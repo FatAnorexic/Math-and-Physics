623fa077f141eb39d189fa6cfcd0d4c73f1b8e92
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-05 17:37:17 -0500

    Add code for bisection method and fix file name error

---
+++ finding_roots/bisection_method.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def f(x):
+    return -4*np.power(x,3)+12*np.power(x,2)+9
+
+def bisection(a, b, err):
+    while(np.abs(a-b)>=err):
+        c=(a+b)/2.0
+        if f(a)*f(c)>err:
+            a=c
+        elif f(a)*f(c)<err:
+            b=c
+
+    return c
+
+test=bisection(-3,4,1e-8)
+print(f'x= [{test}]')
