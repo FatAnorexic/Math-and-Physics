e540df8b8a9c9ccc601b1b758541276db1030c65
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-08 20:01:42 -0500

    Add several examples to bernoulli_diffyq

--- diffy_q/bernoulli_diffyq.py
+++ diffy_q/bernoulli_diffyq.py
@@ -8,10 +8,14 @@ seaborn.set_style("darkgrid", {'grid.color':'.6','grid.style':'dots'})
 def y(x):
     # place your final solution to the equation here
     # return -16/(x**4*limit(x)) #ex1
-    return np.power(((139*np.exp(15*x)-3*np.exp(-2*x))/17), 0.333) #ex2
+    # return np.power(((139*np.exp(15*x)-3*np.exp(-2*x))/17), 0.333) #ex2
+    # return -(2/np.power(limit(x),1/3)) #ex 3
+    return (x**3-2*x**(3/2)+1)/(9*x) #ex4
 def limit(x):
     # place the limiting interval to solve for here
-    return (1+16*np.log(x/2)) #ex1
+    # return (1+16*np.log(x/2)) #ex1
+    # return 4*x-4+5*np.exp(-x) #ex 3
+    return 9*x #ex 4
 def ex_1():
     #set the interval of validity and number of iterations you wish to compute
     x=np.linspace(2*np.exp(-10), 5, 100)
@@ -25,13 +29,25 @@ def ex_2():
     x = np.linspace(-0.2, 0.4, 10000)
     graph_y(x)
 
+def ex_3():
+    xlim=np.linspace(-100,100,100)
+    graph_limit(xlim)
+
+    # x=np.linspace(-12,12,1000)
+    # graph_y(x)
+def ex_4():
+    # xlim=np.linspace(-10,10,100)
+    # graph_limit(xlim)
+
+    x=np.linspace(0.01,10,100)
+    graph_y(x)
 def graph_limit(x):
     # graph the limiting factor here. Often used to get an idea
     #for a range in which to use fsolve or other methods in finding roots
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
-    ax.plot(x, limit(x), color='red', label=r'$$')
+    ax.plot(x, limit(x), color='red', label=r'$4x-4+5e^x$')
 
     ax.spines[['left', 'bottom']].set_position(('data', 0))
     ax.spines[['right', 'top']].set_visible(False)
@@ -45,7 +61,7 @@ def graph_y(x):
     ax = fig.add_subplot(1, 1, 1)
 
     ax.plot(x, y(x), color='red',
-            label=r'$y(x)=\left(\frac{139e^{15x}-3e^{-2x}}{17}\right)^{1/3}$')
+            label=r'$y(x)=y(x)=\frac{x^3-2x^{\frac {3} {2}}+1}{9x}$')
 
     # ax.spines[['left', 'bottom']].set_position('zero')
     # ax.spines[['right', 'top']].set_visible(False)
@@ -58,4 +74,6 @@ def graph_y(x):
 if __name__=='__main__':
     #call your main function here
     # ex_1()
-    ex_2()
+    #ex_2()
+    # ex_3()
+    ex_4()
