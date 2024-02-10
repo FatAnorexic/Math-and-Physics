d807edbd1d909bd6ae8d511ea5d91042fdad6622
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-10 12:42:27 -0500

    Add example 3

--- diffy_q/substitutions.py
+++ diffy_q/substitutions.py
@@ -9,7 +9,8 @@ ax=fig.add_subplot(1,1,1)
 
 def y(x):
     # return -1*np.sqrt(limit(x)/(x**2)) #ex_1_final
-    return x*np.exp((np.log(4)+1)/x-1)
+    # return x*np.exp((np.log(4)+1)/x-1) #ex_2_final
+    return 4*x-(1-9*np.exp(-4*x))/(1+3*np.exp(-4*x)) #ex_3_final
 def limit(x):
     return 228-2*x**4   #ex1_limit
 def eq_1():
@@ -26,6 +27,11 @@ def eq_1():
 def eq_2():
     x=np.linspace(0.3,10,100)
     graph_y(x)
+
+def eq_3():
+    x=np.linspace(-5,5,100)
+    graph_y(x)
+
 def graph_limit(x):
 
     ax.plot(x,limit(x),color='red', label=r'$228-4x^4\geq0$')
@@ -34,13 +40,15 @@ def graph_limit(x):
     plt.show()
 def graph_y(x):
 
-    ax.plot(x, y(x), color='blue', label=r'$y(x)=xe^{\frac{\ln(4)+1}{x}-1}$')
-    seaborn.despine(ax=ax, offset=0)
+    ax.plot(x, y(x), color='blue', label=r'$y(x)=4x-\frac{1-9e^{-4x}}{1+3e^{-4x}}$')
+    # seaborn.despine(ax=ax, offset=0)
     # ax.set_xticks([i*-1 for i in range(11)])
-    ax.set_xticks([i for i in range(11)])
+    ax.spines[['left', 'bottom']].set_position('zero')
+    ax.set_xticks(range(-5,6,1))
     plt.legend(loc='best', prop={'size': 10})
     plt.show()
 
 if __name__=='__main__':
     # eq_1()
-    eq_2()
+    # eq_2()
+    eq_3()
