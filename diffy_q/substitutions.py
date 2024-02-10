9a96a5b2ee9de138dc22720a8dfc27734f125544
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-02-10 11:30:14 -0500

    Add eq_2 and changed ax.set_xticks to use list comprehension

--- diffy_q/substitutions.py
+++ diffy_q/substitutions.py
@@ -8,7 +8,8 @@ fig=plt.figure()
 ax=fig.add_subplot(1,1,1)
 
 def y(x):
-    return -1*np.sqrt(limit(x)/(x**2)) #ex_1_final
+    # return -1*np.sqrt(limit(x)/(x**2)) #ex_1_final
+    return x*np.exp((np.log(4)+1)/x-1)
 def limit(x):
     return 228-2*x**4   #ex1_limit
 def eq_1():
@@ -22,6 +23,9 @@ def eq_1():
     print(y(x))
     graph_y(x)
 
+def eq_2():
+    x=np.linspace(0.3,10,100)
+    graph_y(x)
 def graph_limit(x):
 
     ax.plot(x,limit(x),color='red', label=r'$228-4x^4\geq0$')
@@ -30,11 +34,13 @@ def graph_limit(x):
     plt.show()
 def graph_y(x):
 
-    ax.plot(x, y(x), color='blue', label=r'$y(x)=-\sqrt{\frac{228-2x^4}{x^2}}$')
+    ax.plot(x, y(x), color='blue', label=r'$y(x)=xe^{\frac{\ln(4)+1}{x}-1}$')
     seaborn.despine(ax=ax, offset=0)
-    ax.set_xticks(range(5))
+    # ax.set_xticks([i*-1 for i in range(11)])
+    ax.set_xticks([i for i in range(11)])
     plt.legend(loc='best', prop={'size': 10})
     plt.show()
 
 if __name__=='__main__':
-    eq_1()
+    # eq_1()
+    eq_2()
