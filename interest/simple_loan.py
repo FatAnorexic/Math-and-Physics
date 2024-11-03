76b57f914bcf5d85b93f23af5e1ce6d16824fb45
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2024-11-03 13:58:57 -0500

    Fix and segment sections of the simple loan calc

--- interest/simple_loan.py
+++ interest/simple_loan.py
@@ -10,19 +10,27 @@ def average_depreceation(current, initial, time):
 def deprec_ratio(depreciation, init):
     return depreciation/init*100
 
-def main():
+def bank_take():
     i=float(input("Enter the APR of the loan: "))
     t=int(input("Enter the number of years of the loan: "))
     p=float(input("Enter the principle of the loan amount: "))
+    total_bank_take=future_value(p, i, t)
+    print(f"Bank makes: {total_bank_take:.2f}")
     
+def loss_ratio():
     current_val=float(input("Enter the amount of the car being offered: "))
     init_cost=float(input("Enter the initial price of the car: "))
     time=int(input("How many years have you had the car: "))
+    d_amount=average_depreceation(current_val, init_cost, time)
+    depreciation=deprec_ratio(d_amount, init_cost)
     
-    total_bank_take=future_value(p, i, t)
-    depreciation=(average_depreceation(current_val, init_cost, time), init_cost)
-    
-    print(f"Bank makes: {total_bank_take:.2f}\tAverage Yearly Depreciation: {depreciation:.2f}%")
+    print(f"Average Depreciation: {depreciation:.2f}%")
+
+def main():
+    choice=int(input("Enter (1) for total amount you pay to bank, (2) for the average loss on your vehicle: "))
+    if choice==1: bank_take()
+    elif choice==2: loss_ratio()
+    else: main()
     
 if __name__=="__main__":
     main()
